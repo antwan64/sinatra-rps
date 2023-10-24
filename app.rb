bea220fb386647080fe5520bc9ad8516827994d4
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-10-24 19:42:58 +0000

    Paper and Scissors Routes implemented

--- app.rb
+++ app.rb
@@ -23,3 +23,35 @@ get("/rock") do
   end
   erb(:rock)
 end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:scissors)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:paper)
+end
